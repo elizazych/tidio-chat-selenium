27d2760623b17e95075e56ed048b635a9e929cf6
elizazych <eliza.zych@gmail.com>
2018-11-11 17:29:52 +0100

    Sample tests to verification chat on http://www.tidiochat.com/

---
+++ eclipse-workspace/TidioChatSelenium/src/test/sample/HomepageTest.java
@@ -0,0 +1,64 @@
+package test.sample;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+
+/**
+ * @author Eliza Zych
+ * Verfication chat on https://www.tidiochat.com/
+ *
+ */
+
+class HomepageTest {
+
+	WebDriver driver = new FirefoxDriver();
+	WebElement element;
+	String webSite = "https://www.tidiochat.com/";
+	String iFrame =  "tidio-chat-iframe";
+	String xpathBody = ".//*[@id='button-body']";
+	String xpathChat = "html/body/div/div/div/div[1]";
+	String xpathClose = "html/body/div/div/div/div[1]/div[1]/button";
+	String xpathCloseButton = "html/body/div/div/div/div[1]/div[1]/button[1]";
+	
+	void preconditions() {
+		driver.get(webSite);
+		driver.switchTo().frame(iFrame);
+	}
+	
+	@Test
+	void openChat() {
+	
+		preconditions();
+		driver.findElement(By.xpath(xpathBody)).click();
+		driver.close();
+		
+	}
+
+	@Test
+	void checkCloseButton() {
+		
+		preconditions();
+		driver.findElement(By.xpath(xpathChat)).click();
+		boolean checkButton = driver.findElement(By.xpath(xpathClose)).isDisplayed();
+		driver.close();
+		System.out.println("Button is visible: " + checkButton);
+		
+	}
+	
+	@Test
+	void closeOpenedChat() {
+		
+		preconditions();
+		driver.findElement(By.xpath(xpathBody)).click();
+		driver.findElement(By.xpath(xpathCloseButton)).click();
+		driver.close();
+		
+	}
+	
+}
